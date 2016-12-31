1467361bf5a67b9d27eea0342d6d0b52390d6326
Pedro Lima <pedro.nym@gmail.com>
2016-12-31 13:06:58 +0000

    Started doing partial rendering and updated the API

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
-const path    = require('path'),
-      express = require('express'),
-      exphbs  = require('express-handlebars'),
-      api     = require('./lib/api'),
-      config  = require('./config'),
-      server  = express(),
-      router  = express.Router(),
+const path       = require('path'),
+      express    = require('express'),
+      exphbs     = require('express-handlebars'),
+      api        = require('./lib/api'),
+      config     = require('./config'),
+      server     = express(),
+      router     = express.Router(),
 
-      ENV     = 'dev',
-      PORT    = config.env[ENV].port,
-      FILES   = config.env[ENV].files,
+      ENV        = 'dev',
+      PORT       = config.env[ENV].port,
+      FILES      = config.env[ENV].files,
 
       hbs = exphbs.create({
         extname:       '.hbs',
+        viewsDir:      `${FILES}/templates/pages/`,
         layoutsDir:    `${FILES}/templates/layouts/`,
         partialsDir:   `${FILES}/templates/partials/`,
         defaultLayout: 'master'
       })
 
+// Express - Server Configuration //
 server.use('/', express.static(path.join(__dirname, FILES)))
 server.use(router)
 server.engine('.hbs', hbs.engine)
-
+server.set('port', process.env.PORT || PORT)
 server.set('view engine', '.hbs')
-server.set('views', 'src/templates/views/')
+server.set('views', 'src/templates/')
 
+// Default Route - Renders the Homepage layout //
 router.get('/', function (req, res) {
-  res.render('index')
+  const userData    = api.getUser(),
+        friendsData = api.getFriends()
+
+  res.render('pages/home', {
+    user:    userData,
+    friends: friendsData
+  })
 })
 
-router.get('/api/get', function (req, res) {
-  const data = api.getUser()
-
-  if (data) {
-    res.status(200).json(data)
-  } else {
-    res.status(404).send('404 Oopsie')
-  }
+// Default Route - Renders the Homepage layout //
+router.get('/friend/:id', function (req, res, next) {
+  const friendData  = api.getFriend(req.params.id)
+  res.render('partials/friend-profile', {
+    layout: false,
+    friend: friendData,
+    apiKey: config.MAPS_KEY
+  })
 })
 
-server.listen(PORT, function () {
-  console.log(`Server listening on port: ${PORT}. Environment: ${ENV}`)
+// Get Users Route - Returns the compiled userlist //
+router.get('/api/getFriends', function (req, res, next) {
+  res.render('partials/user-list', {
+    layout: false,
+    users:  api.getFriends()
+  })
+})
+
+router.get('/api/getUser', function (req, res, next) {
+  res.render('partials/usercard', {
+    user: api.getUser()
+  })
+})
+
+// Makes the server listen for requests on the defined port //
+server.listen(server.get('port'), function () {
+  console.log(`Server listening on port: ${server.get('port')}. Environment: ${server.get('env')}`)
 })
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,44 @@
 (function (win, doc) {
-  fetch('/api/get/')
-  .then((response) => {
-    console.log(response)
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+  const u       = win.u,
+        ajax    = win.ajax
+
+  // Starts the app //
+  function init () {
+    u('.friend').on('click', function (e) {
+      e.preventDefault()
+      e.stopPropagation()
+
+      var friendId = u(e.currentTarget).attr('data-uuid')
+
+      getPartial({
+        route:     '/friend/' + friendId,
+        body:      'id=' + friendId,
+        container: u('.main-view')
+      })
+    })
+  }
+
+  // Updates the view //
+  function update (content, container) {
+    u(container).html(content)
+  }
+
+  function getPartial (options) {
+    if (options.route) {
+      ajax(options.route, {
+        body:   options.body || '',
+        method: options.method || 'GET'
+      }, function (err, data) {
+        if (data) {
+          return update(data, options.container)
+        } else {
+          console.log(err)
+        }
+      }, function (xhr) {
+        xhr.responseType = 'text'
+      })
+    }
+  }
+
+  init()
 })(window, document)
